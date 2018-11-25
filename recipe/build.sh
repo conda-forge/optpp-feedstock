8a894ca17627efb57f9839aa7618647df6db903d
Julien Schueller <schueller@phimeca.com>
2018-11-25 12:15:16 +0100

    config.log

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ autoreconf -vfi
 export LDFLAGS="${LDFLAGS} -Wl,-no-undefined"
 ./configure \
   --prefix=$PREFIX --includedir=$PREFIX/include/opt++ \
-  --disable-static --enable-shared
+  --disable-static --enable-shared || cat config.log
 make -j${CPU_COUNT}
 make check -j${CPU_COUNT}
 make install
